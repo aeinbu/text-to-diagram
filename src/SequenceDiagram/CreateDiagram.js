0b7ce054f1b2a0069b5f3f24d0be7510d7fe6abb
Arjan Einbu <arjan@einbu.no>
2021-12-24 00:00:56 +0100

    Refactored svg rendering

--- src/SequenceDiagram/CreateDiagram.js
+++ src/SequenceDiagram/CreateDiagram.js
@@ -9,50 +9,94 @@ export const CreateDiagram = ({ data }) => {
 
 	const arrows = data
 
-	// Layout parameters
+
 	const legendHeight = 30
 	const annotationHeight = 20
-
 	const endpointSpacing = 120
 	const endpointOffset = endpointSpacing / 2
 	const arrowSpacing = annotationHeight + 20
 	const arrowOffset = arrowSpacing / 2
-	const endpointHeight = arrows.length * arrowSpacing
+	const layoutParameters = {
+		legendHeight,
+		annotationHeight,
+		endpointSpacing,
+		endpointOffset,
+		arrowSpacing,
+		arrowOffset
+	}
 
 	return <svg
 		width={endpointSpacing * endpoints.length}
 		height={arrowSpacing * arrows.length + legendHeight * 2}
 		style="border: thin solid lightgrey;"
 	>
-		<defs>
-			<marker id="arrow" markerWidth="30" markerHeight="20" refX="9" refY="3" orient="auto" markerUnits="strokeWidth" viewBox="0 0 20 20">
-				<path d="M0,0 L0,6 L9,3 z" fill="black" />
-			</marker>
-		</defs>
+		<Defs data={undefined} />
+		{endpoints.map((endpoint, ix) => <Endpoint key={ix} name={endpoint} arrowCount={arrows.length} index={ix} layoutParameters={layoutParameters} />)}
+		{arrows.map((arrow, ix) => <Arrow key={ix} arrow={arrow} endpoints={endpoints} layoutParameters={layoutParameters} index={ix}/>)}
+	</svg>
+}
 
-		{endpoints.map((endpoint, ix) => <text
-			x={ix * endpointSpacing + endpointOffset}
+
+export const Defs = (props = {}) => {
+	return <defs>
+		<marker id="arrow" markerWidth="30" markerHeight="20" refX="9" refY="3" orient="auto" markerUnits="strokeWidth" viewBox="0 0 20 20">
+			<path d="M0,0 L0,6 L9,3 z" fill="black" />
+		</marker>
+	</defs>
+}
+
+
+export const Endpoint = ({ name, arrowCount, layoutParameters, index }) => {
+	const { legendHeight, endpointSpacing, arrowSpacing } = layoutParameters
+
+	const lineHeight = arrowCount * arrowSpacing
+	const height = lineHeight + legendHeight * 2
+	const width = endpointSpacing
+	const viewbox = `${-width / 2} 0 ${width} ${height}`
+
+	return <svg
+		width={width} height={height}
+		viewBox={viewbox}
+		x={index * endpointSpacing} y={0}
+	>
+		<text
+			x={0}
 			y={legendHeight / 2}
-			text-anchor="middle" dominant-baseline="middle">{endpoint}</text>)}
+			text-anchor="middle" dominant-baseline="middle">{name}</text>
 
-		{endpoints.map((endpoint, ix) => <text
-			x={ix * endpointSpacing + endpointOffset}
-			y={arrowSpacing * arrows.length + legendHeight * 2 - legendHeight / 2}
-			text-anchor="middle" dominant-baseline="middle">{endpoint}</text>)}
+		<text
+			x={0}
+			y={lineHeight + legendHeight + legendHeight / 2}
+			text-anchor="middle" dominant-baseline="middle">{name}</text>
 
-		{endpoints.map((endpoint, ix) => <line
-			x1={ix * endpointSpacing + endpointOffset} y1={legendHeight}
-			x2={ix * endpointSpacing + endpointOffset} y2={legendHeight + endpointHeight}
-			stroke="red" />)}
+		<line
+			x1={0} y1={legendHeight}
+			x2={0} y2={legendHeight + lineHeight}
+			stroke="purple" />
+	</svg>
+}
 
-		{arrows.map((arrow, ix) => <line
-			x1={endpoints.indexOf(arrow.from.name) * endpointSpacing + endpointOffset} y1={ix * arrowSpacing + arrowOffset + legendHeight}
-			x2={endpoints.indexOf(arrow.to.name) * endpointSpacing + endpointOffset} y2={ix * arrowSpacing + arrowOffset + legendHeight}
-			stroke="black" stroke-width="1" marker-end="url(#arrow)" />)}
 
-		{arrows.map((arrow, ix) => arrow.annotation !== undefined && <text
-			x={endpoints.indexOf(arrow.from.name) + endpoints.indexOf(arrow.to.name) / 2 * endpointSpacing + endpointSpacing}
-			y={ix * arrowSpacing + arrowOffset + legendHeight - annotationHeight / 2}
-			text-anchor="middle" dominant-baseline="middle">{arrow.annotation}</text>)}
+export const Arrow = ({ arrow, endpoints, layoutParameters, index }) => {
+	const { legendHeight, annotationHeight, endpointSpacing, endpointOffset, arrowSpacing, arrowOffset } = layoutParameters
+	const height = arrowSpacing
+	const width = endpointSpacing * endpoints.length
+	const viewbox = `0 ${-(height+annotationHeight)/2} ${width} ${arrowSpacing}`
+
+	return <svg
+		width={width} height={arrowSpacing}
+		viewBox={viewbox}
+		x={0} y={index * arrowSpacing + legendHeight}
+	>
+		<line
+			x1={endpoints.indexOf(arrow.from.name) * endpointSpacing + endpointOffset} y1={0}
+			x2={endpoints.indexOf(arrow.to.name) * endpointSpacing + endpointOffset} y2={0}
+			stroke="black" stroke-width="1" marker-end="url(#arrow)" />
+
+		{arrow.annotation !== undefined && <text
+			x={(endpoints.indexOf(arrow.from.name) + endpoints.indexOf(arrow.to.name)) / 2 * endpointSpacing + endpointSpacing / 2}
+			y={-annotationHeight / 2}
+			text-anchor="middle" dominant-baseline="middle">{arrow.annotation}</text>}
+
 	</svg>
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import { createElement, StatelessProps } from "tsx-create-element";
 import { CreateDiagram as CreateSequenceDiagram } from "./SequenceDiagram/CreateDiagram"
+import { CreatePlayground } from "./Playground/CreatePlayground"
 import sequenceDiagramData from "./SequenceDiagram/sample.json"
 
+const incomingText = `
+client (first) -> server [top]
+server -> remote site [first request]
+server -> logger (last) [third]
+remote site -> server
+server -> remote site [second request]
+remote site -> server
+server -> other remote site [fourth]
+other remote site -> server
+server -> client
+server -> logger
+dill -> dall [hallo]
+client -> remote site
+`
 
-// TODO: Load and parse data at runtime
 document.body.appendChild(<>
 	<h1>Sequence diagram</h1>
 	<CreateSequenceDiagram data={sequenceDiagramData} />
